c7f6e2811c1196b9547f783c20f4d55e9d5df67e
Vikry Surya Pangestu <34179954+vikrysurya24@users.noreply.github.com>
2021-08-09 09:22:59 +0800

    update

--- resources/views/layouts/auth/footer.blade.php
+++ resources/views/layouts/auth/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="py-4 bg-light mt-auto">
     <div class="container-fluid">
        <div class="d-flex align-items-center justify-content-between small">
-          <div class="text-muted font-weight-bold">Created with <i class="bi bi-heart-fill" style="color: red"></i> by Vikry Surya P &middot; Copyright &copy; Your Website {{ date('Y') }}</div>
+          <div class="text-muted font-weight-bold">Created with <i class="bi bi-heart-fill" style="color: red"></i> by Vikry Surya P &middot; Copyright &copy; {{ config('app.name') }}&middot;{{ date('Y') }}</div>
           <div class="font-weight-bold">
              <a href="#">Privacy Policy</a>
              &middot;
